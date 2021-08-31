2f63137a2a08a060cefb6f51ad7aa669f484e99e
Punit Sai Arani <punitsai36@gmail.com>
2021-08-30 17:15:29 -0700

    Add Financials caching

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 
 from polygon_data import Financials
@@ -11,9 +10,15 @@ st.title('Research Stock')
 # Get ticker input
 ticker = st.text_input("Enter Ticker", "AAPL")
 
+
 # Get financials data
-df_financials = Financials(ticker=ticker).get_financials()
-financials_index = df_financials.index.to_list()
+@st.cache
+def get_financials(t):
+    return Financials(ticker=t).get_financials()
+
+
+df_financials = get_financials(ticker)
+metrics = df_financials.index.to_list()
 
 # Display Dataframe
 st.subheader('Financials')
