02806d32198e1b491ec302494a18a51f4def093a
Punit Sai Arani <punitsai36@gmail.com>
2021-08-30 17:36:13 -0700

    Add basic financials plotting

--- main.py
+++ main.py
@@ -8,18 +8,29 @@ st.set_page_config(layout="wide")
 st.title('Research Stock')
 
 # Get ticker input
-ticker = st.text_input("Enter Ticker", "AAPL")
+ticker = st.text_input("Enter Ticker", "AAPL").upper()
 
 
 # Get financials data
-@st.cache
+@st.cache(show_spinner=False)
 def get_financials(t):
     return Financials(ticker=t).get_financials()
 
 
-df_financials = get_financials(ticker)
-metrics = df_financials.index.to_list()
+with st.spinner('Getting Financials...'):
+    df_financials = get_financials(ticker)
 
-# Display Dataframe
+# Display Financials Dataframe
 st.subheader('Financials')
 st.dataframe(df_financials)
+
+# Plot Financials
+metrics = df_financials.index.to_list()
+st.subheader('Plot Financials')
+metrics_selected = st.multiselect('Metrics', metrics)
+
+if not metrics_selected:
+    metrics_selected = ['marketCapitalization']
+
+df_financials_plot = df_financials.copy().loc[metrics_selected].T
+st.line_chart(df_financials_plot)
